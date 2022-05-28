5b53faa41b293b8b57d18891601392866d2b91c3
Max Tochilashvili <100846975+mTochilashvili@users.noreply.github.com>
2022-05-28 18:56:13 +0300

    Add files via upload

---
+++ App/ClockForm.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace ClockCSharp
+{
+    partial class ClockForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(220, 70);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(0, 15);
+            this.label.TabIndex = 0;
+            // 
+            // ClockForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 500);
+            this.Controls.Add(this.label);
+            this.Name = "ClockForm";
+            this.Text = "Clock";
+            this.Load += new System.EventHandler(this.ClockForm_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.ClockForm_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Label label;
+    }
+}
+
---
+++ App/ClockForm.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClockCSharp {
+    public partial class ClockForm : Form {
+        public ClockForm() {
+            InitializeComponent();
+        }
+        private void ClockForm_Load(object sender, EventArgs e) {
+            this.DoubleBuffered = true;
+            Width = 500;
+            Height = 500;
+        }
+        private void ClockForm_Paint(object sender, PaintEventArgs e) {
+            DateTime dt = DateTime.Now;
+            Pen silver_pen = new Pen(Color.Silver, 2);
+            Graphics g = e.Graphics;
+            GraphicsState gs;
+
+            g.TranslateTransform(Width / 2, Height / 2);
+            int Form_Width = Width;
+            int Form_Height = Height;
+            int circle = Form_Width >= Form_Height ? Form_Width : Form_Height;
+            g.DrawEllipse(silver_pen, - circle / 4, - circle / 4, circle / 2, circle / 2);
+            for(int i = 0; i < 12; i++) {
+                if(i == 10 || i == 11)
+                    g.DrawRectangle(silver_pen, circle / 4 * (float)Math.Sin(i * Math.PI / 6) - circle / 60, -circle / 4 * (float)Math.Cos(i * Math.PI / 6) - circle / 50, circle / 20, circle / 20);
+                else
+                    g.DrawRectangle(silver_pen, circle / 4 * (float)Math.Sin(i * Math.PI / 6) - circle / 40, -circle / 4 * (float)Math.Cos(i * Math.PI / 6) - circle / 50, circle / 20, circle / 20);
+                g.DrawString(i.ToString(), new Font("Arial", circle / 30), new SolidBrush(Color.Black), circle / 4 * (float)Math.Sin(i * Math.PI / 6) - circle / 50, -circle / 4 * (float)Math.Cos(i * Math.PI / 6) - circle / 50);
+            }
+            g.DrawLine(new Pen(Color.Red, 4), 0, 0, (int)(Math.Sin(Math.PI / 30 * dt.Second) * circle / 5), - (int)(Math.Cos(Math.PI / 30 * dt.Second) * circle / 5));
+            g.DrawLine(new Pen(Color.Green, 4), 0, 0, (int)(Math.Sin(Math.PI / 30 * (dt.Minute + (float)dt.Second / 60)) * circle / 7), -(int)(Math.Cos(Math.PI / 30 * (dt.Minute + (float)dt.Second / 60)) * circle / 7));
+            g.DrawLine(new Pen(Color.Blue, 4), 0, 0, (int)(Math.Sin(Math.PI / 6 * (dt.Hour + (float)dt.Minute / 60) + (float)dt.Second / 3600) * circle / 9), -(int)(Math.Cos(Math.PI / 6 * (dt.Hour + (float)dt.Minute / 60) + (float)dt.Second / 3600) * circle / 9));
+            gs = g.Save();
+            g.Restore(gs);
+        }
+        private void timer_Tick(object sender, EventArgs e) {
+            int h = DateTime.Now.Hour;
+            int m = DateTime.Now.Minute;
+            int s = DateTime.Now.Second;
+
+            string time = "";
+
+            if (h < 10) {
+                time += "0" + h;
+            } else {
+                time += h;
+            }
+            time += ":";
+            if (m < 10)
+            {
+                time += "0" + m;
+            } else {
+                time += m;
+            }
+            time += ":";
+            if (s < 10)
+            {
+                time += "0" + s;
+            } else {
+                time += s;
+            }
+            label.Text = time;
+            Controls.Add(label);
+            this.Invalidate();
+        }
+    }
+}
